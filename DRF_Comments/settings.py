a31c22ca0a3fa281872815637df65455e312797d
dmitriy <krivosheind28@gmail.com>
2021-09-13 17:13:38 +0300

    end

--- DRF_Comments/settings.py
+++ DRF_Comments/settings.py
@@ -78,17 +78,23 @@ WSGI_APPLICATION = 'DRF_Comments.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'TEST_SM',
+#         'USER': 'dmitriy',
+#         'PASSWORD': '123456db',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'TEST_SM',
-        'USER': 'dmitriy',
-        'PASSWORD': '123456db',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
--- comments/views.py
+++ comments/views.py
@@ -1,10 +1,14 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from django.db.models import Prefetch
+
 from .serializers import *
 
 
 class ArticleViewSet(ModelViewSet):
     """Вывод статей"""
-    queryset = Articles.objects.filter(is_published=True).prefetch_related('comments') #надо оставить комментарии 0 уровня
+    queryset = Articles.objects\
+        .filter(is_published=True)\
+        .prefetch_related(Prefetch('comments', queryset=Comments.objects.filter(level=0))) #надо оставить комментарии 0 уровня
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -16,9 +20,9 @@ class CommentsListViewSet(ModelViewSet):
     def get_queryset(self):
         if self.action == 'list':
             return Comments.objects.filter(is_published=True, level=0)
-        return Comments.objects.filter(id=self.kwargs.get('pk')).prefetch_related('children')
+        return Comments.objects.filter(id=self.kwargs.get('pk'))
 
     def get_serializer_class(self):
-        if self.action == 'list':
-            return CommentsListSerializer
-        return CommentsDetailSerializer
+        if self.action == 'retrieve':
+            return CommentsDetailSerializer
+        return CommentsListSerializer
